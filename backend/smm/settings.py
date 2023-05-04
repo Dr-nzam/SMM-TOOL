0d19a7064bcc1233968837d145566bf8670a9e6a
Dr-nzam <nzambouarmel@gmail.com>
2023-05-04 16:18:59 +0100

    p3

--- backend/smm/settings.py
+++ backend/smm/settings.py
@@ -125,22 +125,15 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = "auth_fb.Utilisateur"
+AUTH_USER_MODEL = "auth_fb.User"
 
 
-REST_FRAMEWORK = { 
-    'DEFAULT_AUTHENTICATION_CLASSES': ( 
-        'rest_framework.authentication.TokenAuthentication', 
-    ), 
-    'DEFAULT_PERMISSION_CLASSES': ( 
-        'rest_framework.permissions.IsAuthenticated', 
-    ), 
-    'DEFAULT_RENDERER_CLASSES': ( 
-        'rest_framework.renderers.JSONRenderer', 
-        'rest_framework.renderers.BrowsableAPIRenderer' 
-    ), 
-    'DEFAULT_PAGINATION_CLASS' : ' rest_framework.pagination.LimitOffsetPagination ', 
-    'PAGE_SIZE' : 10,
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
+    'DEFAULT_RENDERER_CLASSES' : ['rest_framework.renderers.JSONRenderer', 'rest_framework.renderers.BrowsableAPIRenderer'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
